dc6dfc611e0f723d1977e5966bfb4887cf78eb21
Vig1lante <apakieser@gmail.com>
2020-02-24 10:28:17 +0100

    Initial commit - add existing project files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace battle_ships {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Square newSquare = new Square();
+        }
+    }
+}
